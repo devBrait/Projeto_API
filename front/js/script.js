83af4b8f580b6921c46c0c73f9f97bb7db91f448
brait <lorimiervb@gmail.com>
2024-01-22 17:51:22 -0300

    Subindo cadastro.html e outras alterações

--- front/js/script.js
+++ front/js/script.js
@@ -2,6 +2,25 @@ const menorValor = 1
 const maiorValor = 1000
 const numeroSorteado = gerarNumero()
 
+function redirecionarParaLogin() {
+    // Redirecionar para a página de login
+    window.location.href = 'login.html';
+}
+
+function mudarCursor() {
+    // Mudar o cursor do mouse para 'pointer'
+    document.getElementById('LoginHelp').style.cursor = 'pointer';
+}
+
+function mudarCursorLogin() {
+    // Mudar o cursor do mouse para 'pointer'
+    document.getElementById('CadastroHelp').style.cursor = 'pointer';
+}
+
+function redirecionarParaCadastro() {
+    // Redirecionar para a página de login
+    window.location.href = 'cadastro.html';
+}
 
 function gerarNumero(){
     return parseInt(Math.random() * maiorValor + 1)
@@ -13,3 +32,4 @@ const elementoMenorValor = document.getElementById('menor-valor')
 elementoMenorValor.innerHTML = menorValor
 const elementoMaiorValor = document.getElementById('maior-valor')
 elementoMaiorValor.innerHTML = maiorValor
+
